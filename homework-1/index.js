9746857a417e02c69d30f587bdd3befd8ddad3d3
davcevva <163914445+davcevva@users.noreply.github.com>
2024-06-22 01:52:11 +0200

    change

--- homework-1/index.js
+++ homework-1/index.js
@@ -10,22 +10,23 @@ function calculateGrades() {
   const results = [];
   for (let i = 0; i < students.length; i++) {
     const score = students[i].scores;
-    for (let j = 0; j < score.length; j++) {
-      const num = score[j];
-      let grade;
-      if (num >= 90) {
-        grade = "A";
-      } else if (num >= 80) {
-        grade = "B";
-      } else if (num >= 70) {
-        grade = "C";
-      } else if (num >= 60) {
-        grade = "D";
-      } else {
-        grade = "F";
-      }
-      results.push({ name: students[i].name, grade: grade });
+    const sum = score.reduce((acc, curr) => acc + curr, 0);
+    // console.log(sum);
+    let total = sum / score.length;
+    // console.log(total)
+    let grade;
+    if (total >= 90) {
+      grade = "A";
+    } else if (total >= 80) {
+      grade = "B";
+    } else if (total >= 70) {
+      grade = "C";
+    } else if (total >= 60) {
+      grade = "D";
+    } else {
+      grade = "F";
     }
+    results.push({ name: students[i].name, grade: grade });
   }
   console.log(results);
 }
